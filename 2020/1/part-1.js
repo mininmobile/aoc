154300afe06c475a53b9f16f321ce05b08d8c553
not ervin <notminin@pm.me>
2020-12-01 22:16:17 +0000

    shorten

--- 2020/1/part-1.js
+++ 2020/1/part-1.js
@@ -1,11 +1,7 @@
 let data = require("fs").readFileSync("input.txt", "utf8");
 let numbers = data.split("\n").map(x => parseInt(x)).filter(x => !isNaN(x));
 
-let result;
-
 for (let a of numbers)
 	for (let b of numbers)
 		if (a + b == 2020)
-			result = a * b;
-
-console.log(result || "unable to solve!");
+			console.log(a * b);
