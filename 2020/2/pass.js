1d80f20297758986a278bb8d9e23cc35fbefcf1b
not ervin <notminin@pm.me>
2020-12-02 17:46:57 +0000

    part 2

--- 2020/2/pass.js
+++ 2020/2/pass.js
@@ -2,15 +2,17 @@ const { parse } = require("path");
 
 let data = require("fs").readFileSync("input.txt", "utf8");
 let rawPasswords = data.split("\n").map(x => x.split(/(: |-| )/g).filter(x => ![": ", " ", "-"].includes(x))).filter(x => x.length == 4);
-let goodPasswords = [];
+let goodPasswords;
 
+// part 1
+goodPasswords = [];
 for (let pw of rawPasswords) {
 	let c = count(pw[3], pw[2]);
 	if (c >= parseInt(pw[0]) && c <= parseInt(pw[1]))
 		goodPasswords.push(pw[3]);
 }
 
-console.log(goodPasswords.length);
+console.log("part 1:", goodPasswords.length);
 
 function count(string, char) {
 	let count = 0;
@@ -21,3 +23,14 @@ function count(string, char) {
 
 	return count;
 }
+
+// part 2
+goodPasswords = [];
+for (let pw of rawPasswords) {
+	let a = pw[3].charAt(parseInt(pw[0]) - 1) == pw[2];
+	let b = pw[3].charAt(parseInt(pw[1]) - 1) == pw[2];
+	if (a != b)
+		goodPasswords.push(pw[3]);
+}
+
+console.log("part 2:", goodPasswords.length);
