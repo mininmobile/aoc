800cbb5c0dab600c4fce89d2aadbf4bde593e8a8
not ervin <notminin@pm.me>
2020-12-03 21:44:35 +0000

    "p2" but it doesn't work :<

--- 2020/3/tobo.js
+++ 2020/3/tobo.js
@@ -2,9 +2,23 @@ let data = require("fs").readFileSync("input.txt", "utf8");
 let map = data.split("\n");
 let w = map[0].length;
 
+
 // p1
 console.log("part 1:", ride(3, 1));
 
+// p2
+let trees = [
+	[1, 1],
+	[3, 1],
+	[5, 1],
+	[7, 1],
+	[1, 2],
+].map(x => ride.apply(this, x));
+let total = trees.reduce((a, b) => a * b);
+
+console.log("part 2:", trees, total);
+
+// util
 function ride(right, down) {
 	let trees = 0;
 
